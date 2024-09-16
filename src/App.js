dd97e9235ebbcf88931b1476bfdd0592331cd405
NipunaDakshina <87487225+NipunaDakshina@users.noreply.github.com>
2024-09-16 16:21:04 +0530

    add navigation bar with routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-
-import './App.css';
-import Header from './components/Header';
-import Main from './components/Main';
-import About from './components/About';
-import Skill from './components/Skill';
-import Project from './components/Project';
-import Dashboard from './components/Dashboard';
-import Contact from './components/Contact';
+import "./App.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import About from "./components/About";
+import Skill from "./components/Skill";
+import Project from "./components/Project";
+import Dashboard from "./components/Dashboard";
+import Contact from "./components/Contact";
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <div className='container'>
-          <div><Header/></div>
-          <div><Main/></div>
-          <div><About/></div>
-          <div><Skill/></div>
-          <div><Project/></div>
-          <div><Dashboard/></div>
-          <div><Contact/></div>
+    <div>
+      <div className="container">
+        <div>
+          <Header />
+        </div>
+        <Routes>
+          <Route path="/" element={<Main />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/skill" element={<Skill />}></Route>
+          <Route path="/project" element={<Project />}></Route>
+          <Route path="/dashboard" element={<Dashboard />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+        </Routes>
       </div>
     </div>
   );
